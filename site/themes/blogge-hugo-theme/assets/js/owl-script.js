663fda0e2fa5d7795c293615abe5b634838cbf59
matthh9797 <matthewh@netextremity.com>
2023-02-22 09:51:29 +0800

    update article

--- site/themes/blogge-hugo-theme/assets/js/owl-script.js
+++ site/themes/blogge-hugo-theme/assets/js/owl-script.js
@@ -1,7 +1,7 @@
 $(".owl-carousel").owlCarousel({
-    autoplay: true,
-    autoplayhoverpause: true,
-    autoplaytimeout: 100,
+    // autoplay: true,
+    // autoplayhoverpause: true,
+    // autoplaytimeout: 100,
     items: 2,
     nav: true,
     loop: true,
